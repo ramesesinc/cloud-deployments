0d559650b2afe6f1753c110b309919f8e79aac85
jzamss@gmail.com <jzamss@gmail.com>
2020-10-12 10:51:30 +0800

    cloud-filipizen-server: initial

---
+++ cloud-filipizen-server/client/precache-manifest.cecbf1c7d6a0412debbf984ed689362b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2536d4a1f4b758fd9938f8ff5c9197fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aeb54e2abf8266d81b7",
+    "url": "/static/css/main.0b610b63.chunk.css"
+  },
+  {
+    "revision": "fccfa765292cd87f5274",
+    "url": "/static/js/2.5cda434f.chunk.js"
+  },
+  {
+    "revision": "f5d048f374f01232f38874cede0681f8",
+    "url": "/static/js/2.5cda434f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aeb54e2abf8266d81b7",
+    "url": "/static/js/main.6a05418b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5fd85b9e136074ed166f46c783115828",
+    "url": "/static/media/filipizen.5fd85b9e.svg"
+  }
+]);
