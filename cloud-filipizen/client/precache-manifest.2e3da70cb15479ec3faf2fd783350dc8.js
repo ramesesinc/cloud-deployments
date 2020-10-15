fa4c59455385a3af357fe71da10fc17d880207d6
jzamss@gmail.com <jzamss@gmail.com>
2020-10-15 14:05:01 +0800

    cloud-filipizen: success and error page update cloud-filipizen: activate wateworks billing

--- cloud-filipizen/client/precache-manifest.2e3da70cb15479ec3faf2fd783350dc8.js
+++ cloud-filipizen/client/precache-manifest.2e3da70cb15479ec3faf2fd783350dc8.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d518c141ee4d0d2d8eecae880198b47c",
+    "revision": "a0fdc06d7fc836a3618fd46cc75f0f29",
     "url": "/index.html"
   },
   {
-    "revision": "22996a5b7803602afe14",
+    "revision": "c43ffd6d8c53cb51374e",
     "url": "/static/css/2.237e9ac5.chunk.css"
   },
   {
-    "revision": "fe6502ad0600a68c3bf7",
+    "revision": "3c2706d1d1a6239484f4",
     "url": "/static/css/main.96813bc5.chunk.css"
   },
   {
-    "revision": "22996a5b7803602afe14",
-    "url": "/static/js/2.962647b4.chunk.js"
+    "revision": "c43ffd6d8c53cb51374e",
+    "url": "/static/js/2.c9bb1e44.chunk.js"
   },
   {
-    "revision": "f5d048f374f01232f38874cede0681f8",
-    "url": "/static/js/2.962647b4.chunk.js.LICENSE.txt"
+    "revision": "640e709bf88d8f79b9331e473fbe5a65",
+    "url": "/static/js/2.c9bb1e44.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fe6502ad0600a68c3bf7",
-    "url": "/static/js/main.b29796f5.chunk.js"
+    "revision": "3c2706d1d1a6239484f4",
+    "url": "/static/js/main.be6dbe85.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
--- cloud-filipizen/client/service-worker.js
+++ cloud-filipizen/client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3772e61bc09240f16e11d756e6fe5442.js"
+  "/precache-manifest.2e3da70cb15479ec3faf2fd783350dc8.js"
 );
 
 self.addEventListener('message', (event) => {
