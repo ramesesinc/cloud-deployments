4be61c987de4a79035598006d0b13018b951ac14
worgflores <worgflores@gmail.com>
2020-10-16 09:33:21 +0800

    updated

--- cloud-partner/modules/cloud-partner-services.jar/sql/gdx_partner.sql
+++ cloud-partner/modules/cloud-partner-services.jar/sql/gdx_partner.sql
@@ -1,12 +1,9 @@
 ## updated
 [getList]
 select 
-	p.id, p.name, p.title, 
-	p.clusterid, p.type, p.subtype, p.state, 
-	p.includeservices, p.excludeservices, 
-	c.objid as group_objid, 
-	c.title as group_title, 
-	c.name as group_name 
+	p.id, p.name, p.title, p.clusterid, p.type, p.subtype, p.state, 
+	p.includeservices, p.excludeservices, p.email, p.phoneno, 
+	c.objid as group_objid, c.title as group_title, c.name as group_name 
 from partner p 
 	inner join partner_cluster c on c.objid = p.clusterid 
 order by c.name, length(p.id), p.id 
