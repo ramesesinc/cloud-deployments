8302e12355b892b5e49e7c43c957db29d2b9fabd
jzamss@gmail.com <jzamss@gmail.com>
2020-11-28 11:31:52 +0800

    obo updates

---
+++ cloud-filipizen/client/precache-manifest.8ac18c4c8c27e33f70e981dce44e3134.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09dd9c483aaea34eb19ea5f9034a4adb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec145c8175686e55eee2",
+    "url": "/static/css/2.c34e91d2.chunk.css"
+  },
+  {
+    "revision": "e47cc588bcdf28006fe4",
+    "url": "/static/css/main.8be6ed9c.chunk.css"
+  },
+  {
+    "revision": "ec145c8175686e55eee2",
+    "url": "/static/js/2.ad482e09.chunk.js"
+  },
+  {
+    "revision": "ebba8197eff93308c12f1a9f97fa4424",
+    "url": "/static/js/2.ad482e09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c2a17c2bb220f18bc05",
+    "url": "/static/js/3.36f47deb.chunk.js"
+  },
+  {
+    "revision": "14167ff9c113dc64d1ac",
+    "url": "/static/js/4.e3eb195c.chunk.js"
+  },
+  {
+    "revision": "f43d0cb0548eacbe18ec",
+    "url": "/static/js/5.ff342339.chunk.js"
+  },
+  {
+    "revision": "58b876e342b60699f44e",
+    "url": "/static/js/6.ac862f32.chunk.js"
+  },
+  {
+    "revision": "e47cc588bcdf28006fe4",
+    "url": "/static/js/main.b59b08b5.chunk.js"
+  },
+  {
+    "revision": "6361dc9e19d65bcb56a3",
+    "url": "/static/js/runtime-main.f58ac7aa.js"
+  },
+  {
+    "revision": "5fd85b9e136074ed166f46c783115828",
+    "url": "/static/media/filipizen.5fd85b9e.svg"
+  }
+]);
