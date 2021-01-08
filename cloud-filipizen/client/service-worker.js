17a4740afbd99c52573f3035b972913b8d155815
jzamss@gmail.com <jzamss@gmail.com>
2021-01-08 21:44:34 +0800

    cloud-filipizen: obo: buildingpermit updates

--- cloud-filipizen/client/service-worker.js
+++ cloud-filipizen/client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5f69be7cc08f9998530e204b1793a6f5.js"
+  "/precache-manifest.43c2f1db3e5e0d14ef6756d781fca79c.js"
 );
 
 self.addEventListener('message', (event) => {
