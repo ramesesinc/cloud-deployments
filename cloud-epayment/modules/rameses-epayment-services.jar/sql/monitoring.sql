e9c3615442b02765f400af357ef3c68d10d0009e
worgflores <worgflores@gmail.com>
2021-03-01 18:37:45 +0800

    cloud-epayment: updated

---
+++ cloud-epayment/modules/rameses-epayment-services.jar/sql/monitoring.sql
@@ -0,0 +1,14 @@
+[getPartners]
+select distinct partnerid as id
+from payment_partner_option
+
+[getTxnCounts]
+select 
+	pp.orgcode,
+	p.paypartnerid,
+	count(*) as txncount
+from paymentorder_paid pp 
+inner join payment p on pp.objid = p.paymentrefid
+where p.txndate >= $P{fromdate} and p.txndate < $P{todate}
+group by pp.orgcode, paypartnerid
+
