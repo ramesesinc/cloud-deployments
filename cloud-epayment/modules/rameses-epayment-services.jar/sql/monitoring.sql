8440413fa8e0cefcde5ef312fdb259a41614183d
worgflores <worgflores@gmail.com>
2021-03-11 22:51:31 +0800

    cloud-epayment: updated

--- cloud-epayment/modules/rameses-epayment-services.jar/sql/monitoring.sql
+++ cloud-epayment/modules/rameses-epayment-services.jar/sql/monitoring.sql
@@ -12,3 +12,9 @@ inner join payment p on pp.objid = p.paymentrefid
 where p.txndate >= $P{fromdate} and p.txndate < $P{todate}
 group by pp.orgcode, paypartnerid
 
+[findYearRange]
+select 
+	min(year(txndate)) as minyear, 
+	max(year(txndate)) as maxyear 
+from paymentorder_paid
+
