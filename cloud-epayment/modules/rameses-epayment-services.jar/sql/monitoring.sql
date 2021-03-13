7da3c2ffc700dc43294024258ff0719aa7ec1275
Jessie Zamora <jzamss@gmail.com>
2021-03-13 10:24:05 +0800

    cloud-epayment: monitoring: support totals

--- cloud-epayment/modules/rameses-epayment-services.jar/sql/monitoring.sql
+++ cloud-epayment/modules/rameses-epayment-services.jar/sql/monitoring.sql
@@ -12,6 +12,16 @@ inner join payment p on pp.objid = p.paymentrefid
 where p.txndate >= $P{fromdate} and p.txndate < $P{todate}
 group by pp.orgcode, paypartnerid
 
+[getTotalTxnCounts]
+select 
+	p.paypartnerid,
+	count(*) as txncount
+from paymentorder_paid pp 
+inner join payment p on pp.objid = p.paymentrefid
+where p.txndate >= $P{fromdate} and p.txndate < $P{todate}
+and pp.orgcode <> '000'
+group by paypartnerid
+
 [findYearRange]
 select 
 	min(year(txndate)) as minyear, 
