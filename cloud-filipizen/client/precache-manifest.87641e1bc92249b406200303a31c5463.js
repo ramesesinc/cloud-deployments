62eab8c26764a6ef5027e5474da0ad98343f39cd
Jessie Zamora <jzamss@gmail.com>
2021-06-02 14:15:48 +0800

    cloud-filipizen: obo: services title updates

--- cloud-filipizen/client/precache-manifest.87641e1bc92249b406200303a31c5463.js
+++ cloud-filipizen/client/precache-manifest.87641e1bc92249b406200303a31c5463.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "52aa8a428d4edfbad3cd2e5099b03b45",
+    "revision": "589f43b790edcdeb7439632b2f30ac20",
     "url": "/index.html"
   },
   {
-    "revision": "92f68748e20de1d18660",
+    "revision": "5495b63aef5c3db5adb4",
     "url": "/static/css/2.826a7d64.chunk.css"
   },
   {
-    "revision": "447e9e3ee86ae9264b71",
+    "revision": "dfdad87089bdce6b585d",
     "url": "/static/css/main.004b194e.chunk.css"
   },
   {
-    "revision": "92f68748e20de1d18660",
-    "url": "/static/js/2.3d9789a0.chunk.js"
+    "revision": "5495b63aef5c3db5adb4",
+    "url": "/static/js/2.04f4c8c3.chunk.js"
   },
   {
     "revision": "35404924d101c4742361a5c4a5f250c5",
-    "url": "/static/js/2.3d9789a0.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.04f4c8c3.chunk.js.LICENSE.txt"
   },
   {
     "revision": "732f6315c21add4fcd06",
     "url": "/static/js/3.fdcb1e38.chunk.js"
   },
   {
-    "revision": "b34d5b402dc962759945",
-    "url": "/static/js/4.aa4e7a2d.chunk.js"
+    "revision": "60add6e7adac07899079",
+    "url": "/static/js/4.05876d24.chunk.js"
   },
   {
     "revision": "13acbdbdfa6562d7da91",
@@ -36,12 +36,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/6.1e32430c.chunk.js"
   },
   {
-    "revision": "447e9e3ee86ae9264b71",
-    "url": "/static/js/main.b6126d1c.chunk.js"
+    "revision": "dfdad87089bdce6b585d",
+    "url": "/static/js/main.33a40a43.chunk.js"
   },
   {
-    "revision": "34adce967a01101dc630",
-    "url": "/static/js/runtime-main.73148f88.js"
+    "revision": "c203740a126e8504a5b4",
+    "url": "/static/js/runtime-main.c4503417.js"
   },
   {
     "revision": "5fd85b9e136074ed166f46c783115828",
